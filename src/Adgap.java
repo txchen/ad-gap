b22d2674a4aba7c55588be9178f8b70d3be1081e
Tianxiang Chen <txchendev@gmail.com>
2016-07-01 13:40:23 -0700

    try to implement a callback from java to js

--- src/Adgap.java
+++ src/Adgap.java
@@ -3,10 +3,20 @@ package com.txchen.adgap;
 import org.apache.cordova.*;
 import android.content.Context;
 import android.telephony.TelephonyManager;
+import android.content.BroadcastReceiver;
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
+import java.util.Timer;
+import java.util.TimerTask;
+import android.util.Log;
 
 public class Adgap extends CordovaPlugin {
+    private static final String LOG_TAG = "Adgap";
+
+    BroadcastReceiver receiver;
+
+    private CallbackContext _delayedCallbackContext;
 
     @Override
     public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
@@ -19,8 +29,43 @@ public class Adgap extends CordovaPlugin {
               callbackContext.error(e.toString());
               return false;
             }
+        } else if (action.equals("delayedEvent")) {
+            _delayedCallbackContext = callbackContext;
+            Log.e(LOG_TAG, "I am here1!!");
+            new Timer().schedule(
+                new TimerTask() {
+                    @Override
+                    public void run() {
+                        Log.e(LOG_TAG, "I am here2!!");
+                        PluginResult result = new PluginResult(PluginResult.Status.OK, buildDummyEvent(54321));
+                        result.setKeepCallback(true);
+                        _delayedCallbackContext.sendPluginResult(result);
+
+                        Log.e(LOG_TAG, "I am here3!!");
+                        result = new PluginResult(PluginResult.Status.OK, buildDummyEvent(98765));
+                        result.setKeepCallback(true);
+                        _delayedCallbackContext.sendPluginResult(result);
+                    }
+                },
+                3000
+            );
+            // dont return any result now
+            PluginResult pluginResult = new PluginResult(PluginResult.Status.NO_RESULT);
+            pluginResult.setKeepCallback(true);
+            callbackContext.sendPluginResult(pluginResult);
+            return true;
         } else {
             return false;
         }
+    }
+
+    private JSONObject buildDummyEvent(int data) {
+        JSONObject obj = new JSONObject();
+        try {
+            obj.put("val", data);
+        } catch (JSONException e) {
+            Log.e(LOG_TAG, e.getMessage(), e);
+        }
+        return obj;
     }
 }
--- www/adgap.js
+++ www/adgap.js
@@ -2,6 +2,25 @@
 
 module.exports = {
     getIMEI: function (successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "Adgap", "getIMEI", []);
-    }
-};
+      cordova.exec(successCallback, errorCallback, 'Adgap', 'getIMEI', [])
+    },
+
+    delayedEvent: function () {
+      cordova.exec(function (status) {
+          if (status) {
+            console.log('got delayedEvent from java')
+            console.log(status)
+            cordova.fireWindowEvent("delayedevent", status);
+          } else {
+            console.log('got empty status from java')
+          }
+        },
+        function (e) {
+          console.log('Error setup delayed event ' + e)
+        },
+        'Adgap',
+        'delayedEvent',
+        []
+      )
+    },
+}
